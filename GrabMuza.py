33849e2cab028edbc86bf4038662862987b2eb1d
Wasiliy Besedin <besco@yabesco.ru>
2016-09-19 17:44:59 +0300

    Cosmos

--- GrabMuza.py
+++ GrabMuza.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 # coding: utf8
 
-import sys, time, os
+from datetime import datetime, date, time
 from grab import Grab
-import logging
-from pprint import pprint
 
+# import logging
+# from pprint import pprint
 # logging.basicConfig(level=logging.DEBUG)
 
 url = "http://www.muzadisk.ru"
 tickets = "/tickets.html"
 
-months = dict()
-months['января'] = "01"
-months['февраля'] = "02"
-months['марта'] = "03"
-months['апреля'] = "04"
-months['мая'] = "05"
-months['июня'] = "06"
-months['июля'] = "07"
-months['августа'] = "08"
-months['сентября'] = "09"
-months['октября'] = "10"
-months['ноября'] = "11"
-months['декабря'] = "12"
-
-
 def parse(poster_div):
+    months = dict()
+    months['января'] = 1
+    months['февраля'] = 2
+    months['марта'] = 3
+    months['апреля'] = 4
+    months['мая'] = 5
+    months['июня'] = 6
+    months['июля'] = 7
+    months['августа'] = 8
+    months['сентября'] = 9
+    months['октября'] = 10
+    months['ноября'] = 11
+    months['декабря'] = 12
+
+    cur_year = datetime.now().timetuple()[0]
     event = dict()
     values_list = list()
     values_list.append(url+poster_div.select('p//img').attr('src'))
+    lenps = len(poster_div._node.xpath('p'))
+    #print(lenps)
     for ps in poster_div._node.xpath('p'):
         if ps.getchildren():
             for element in ps.getchildren():
@@ -41,8 +43,18 @@ def parse(poster_div):
                                     for element4 in element3.getchildren():
                                         if element4.getchildren():
                                             for element5 in element4.getchildren():
-                                                if element5.text is not None:
-                                                    values_list.append(element5.text)
+                                                if element5.getchildren():
+                                                    for element6 in element5.getchildren():
+                                                        if element6.getchildren():
+                                                            for element7 in element6.getchildren():
+                                                                if element7.text is not None:
+                                                                    values_list.append(element7.text)
+                                                        else:
+                                                            if element6.text is not None:
+                                                                values_list.append(element6.text)
+                                                else:
+                                                    if element5.text is not None:
+                                                        values_list.append(element5.text)
                                         else:
                                             if element4.text is not None:
                                                 values_list.append(element4.text)
@@ -62,21 +74,42 @@ def parse(poster_div):
         if value is '\xa0':
             values_list.pop(i)
         i += 1
-    dates = values_list[2].strip('.').split()
-    event['img'] = values_list[0]
-    event['band'] = values_list[1]
-    event['day'] = dates[0]
-    event['month'] = months[dates[1]]
-    event['year'] = '2016'
-    event['place'] = values_list[3].strip()
-    event['addr'] = values_list[4]
-    event['price'] = values_list[5]
-    return event
+    #print(values_list)
+    if values_list[2] is not None:
+        dates = values_list[2].split()
+        if dates[0].isnumeric() and lenps >= 6:
+            event['img'] = values_list[0]
+            event['band'] = values_list[1]
+            event['day'] = dates[0].strip(',.')
+            event['month'] = months[dates[1].strip(',.')]
+            if len(dates) >= 3:
+                event['year'] = dates[2]
+            else:
+                event['year'] = cur_year
+            event['place'] = values_list[3].strip()
+            event['addr'] = values_list[4]
+            event['price'] = values_list[5]
+            return event
+        else:
+            #print(values_list)
+            pass
+    else:
+        #print(values_list)
+        pass
+
 
 g = Grab()
-g.go(url+tickets)
-posters = g.doc.select('//div[@class="dd-article"]')
+end_page = False;
+while end_page == False:
+    g.go(url+tickets)
+    posters = g.doc.select('//div[@class="dd-article"]')
+    pages = g.doc.select('//div[@class="dd-pager"]//a[@title="Вперёд"]');
+    try:
+        tickets = pages.attr('href')
+    except:
+        end_page=True
 
-for poster in posters:
-    parsed_event = parse(poster)
-    print(parsed_event)
+    for poster in posters:
+        parsed_event = parse(poster)
+        if parsed_event is not None:
+            print(parsed_event)
